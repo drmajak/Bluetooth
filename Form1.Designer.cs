e509cca200d82983235736f3890054793b04f429
Darren Majak <drmajak@ualberta.ca>
2017-02-21 15:23:24 -0700

    initial commit
    Signed-off-by: Darren Majak <drmajak@ualberta.ca>

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Bluetooth_Connect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Connect = new System.Windows.Forms.Button();
+            this.AvailableDevices = new System.Windows.Forms.ListBox();
+            this.Refresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Connect
+            // 
+            this.Connect.Location = new System.Drawing.Point(12, 118);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(75, 23);
+            this.Connect.TabIndex = 0;
+            this.Connect.Text = "Connect";
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // AvailableDevices
+            // 
+            this.AvailableDevices.FormattingEnabled = true;
+            this.AvailableDevices.Location = new System.Drawing.Point(12, 12);
+            this.AvailableDevices.Name = "AvailableDevices";
+            this.AvailableDevices.Size = new System.Drawing.Size(120, 95);
+            this.AvailableDevices.TabIndex = 1;
+            this.AvailableDevices.SelectedIndexChanged += new System.EventHandler(this.AvailableDevices_SelectedIndexChanged);
+            // 
+            // Refresh
+            // 
+            this.Refresh.Location = new System.Drawing.Point(105, 119);
+            this.Refresh.Name = "Refresh";
+            this.Refresh.Size = new System.Drawing.Size(75, 23);
+            this.Refresh.TabIndex = 2;
+            this.Refresh.Text = "Refresh";
+            this.Refresh.UseVisualStyleBackColor = true;
+            this.Refresh.Click += new System.EventHandler(this.Refresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Refresh);
+            this.Controls.Add(this.AvailableDevices);
+            this.Controls.Add(this.Connect);
+            this.Name = "Form1";
+            this.Text = "Bluetooth Connect";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.ListBox AvailableDevices;
+        private System.Windows.Forms.Button Refresh;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InTheHand.Net.Sockets;
+BluetoothClient bc = new BluetoothClient(); 
+
+namespace Bluetooth_Connect
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void Populate_Available_Devices()
+        { }
+        private void AvailableDevices_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Refresh_Click(object sender, EventArgs e)
+        {
+            List<Device> devices = new List<Device>();
+            BluetoothClient bc = new BluetoothClient();
+            BluetoothDeviceInfo[] array = bc.DiscoverDevices();
+            int count = array.Length;
+            for (int i = 0; i < count; i++)
+            {
+                Device device = new Device(array[i]);
+                devices.Add(device);
+                AvailableDevices.Items.Add(device); //Add Devices to List
+            }
+
+        }
+        public class Device
+{
+    public string DeviceName { get; set; }
+    public bool Authenticated { get; set; }
+    public bool Connected { get; set; }
+    public ushort Nap { get; set; }
+    public uint Sap { get; set; }
+    public DateTime LastSeen { get; set; }
+    public DateTime LastUsed { get; set; }
+    public bool Remembered { get; set; }
+ 
+    public Device(BluetoothDeviceInfo device_info)
+    {
+        this.Authenticated = device_info.Authenticated;
+        this.Connected = device_info.Connected;
+        this.DeviceName = device_info.DeviceName;
+        this.LastSeen = device_info.LastSeen;
+        this.LastUsed = device_info.LastUsed;
+        this.Nap = device_info.DeviceAddress.Nap;
+        this.Sap = device_info.DeviceAddress.Sap;
+        this.Remembered = device_info.Remembered;
+    }
+ 
+    public override string ToString()
+    {
+        return this.DeviceName;
+    }
+}
+
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            if(AvailableDevices.SelectedIndex==-1)
+            {
+                DialogResult DB=MessageBox.Show("Please select a device from the list","Select a Device",MessageBoxButtons.OK);
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
